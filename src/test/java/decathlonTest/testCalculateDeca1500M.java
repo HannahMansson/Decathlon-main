b9258d1aa41c44744f7d33121952be87e5400a71
PC <igormaric84@hotmail.com>
2022-10-10 00:05:54 +0200

    Test för decathlon.

---
+++ src/test/java/decathlonTest/testCalculateDeca1500M.java
@@ -0,0 +1,32 @@
+package decathlonTest;
+
+import common.CalcTrackAndField;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class testCalculateDeca1500M {
+    @Test
+    public void testDeca1500M() {
+//Arrange
+        double A = 0.03768;
+        double B = 480;
+        double C = 1.85;
+        double time= 6;
+
+        CalcTrackAndField calc = new CalcTrackAndField();
+
+        //Act
+        int actual = calc.calculateTrack(A,B,C,time);
+
+
+
+        // Assert Assertion;
+        int expected = 3359;
+
+
+        assertEquals(expected, actual);
+
+
+    }
+}
