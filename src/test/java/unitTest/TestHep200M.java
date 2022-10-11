aebc6a09cbdbad05e3e1e6df77f9ca911a16ddfc
IsabelleBellarp <isabellebellarp@gmail.com>
2022-10-11 14:46:10 +0200

    Test

--- src/test/java/unitTest/TestHep200M.java
+++ src/test/java/unitTest/TestHep200M.java
@@ -10,7 +10,7 @@ public class TestHep200M {
     public void testHep200M(){
 
         //Arrange
-        double A = 4.99087; //Test Test
+        double A = 4.99087;
         double B = 42.5;
         double C = 1.81;
         double time = 20.5;
